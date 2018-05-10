fc82b4808ca6cc258680da2b1147a2f3cf56795f
Andrey Starostenko <monro86dev@gmail.com>
2018-05-10 16:05:32 +0300

    initial commit

---
+++ StringMagic/Source/StringMagic.swift
@@ -0,0 +1,28 @@
+//
+//  StringMagic.swift
+//  StringMagic
+//
+//  Created by Andrey Starostenko on 10.05.2018.
+//  Copyright © 2018 Andrey Starostenko. All rights reserved.
+//
+
+import Foundation
+
+private extension Array {
+    var shuffled: Array {
+        var array = self
+
+        indices.dropLast().forEach {
+            guard case let index = Int(arc4random_uniform(UInt32(count - $0))) + $0, index != $0 else { return }
+            array.swapAt($0, index)
+        }
+
+        return array
+    }
+}
+
+public extension String {
+    var jumble: String {
+        return String(Array(self).shuffled)
+    }
+}
---
+++ StringMagic/StringMagic.h
@@ -0,0 +1,19 @@
+//
+//  StringMagic.h
+//  StringMagic
+//
+//  Created by Andrey Starostenko on 10.05.2018.
+//  Copyright © 2018 Andrey Starostenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StringMagic.
+FOUNDATION_EXPORT double StringMagicVersionNumber;
+
+//! Project version string for StringMagic.
+FOUNDATION_EXPORT const unsigned char StringMagicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringMagic/PublicHeader.h>
+
+
